d96ecb0bfdf95f6eace4f9791c7ccd94737cbda9
Deepak Kumar <dgupta330@gmail.com>
2020-08-21 12:57:46 +0530

    First commit

---
+++ src/main/java/com/example/AddController.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class AddController {
+    public ModelAndView add(HttpServletRequest request, HttpServletResponse response)
+    {
+        int i=Integer.parseInt(request.getParameter("t1"));
+        int j=Integer.parseInt(request.getParameter("t1"));
+        int k=i+j;
+
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("display.jsp");
+
+        return mv;
+    }
+}
